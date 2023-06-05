34434a35cf38f6fa5c67fed51a4a18f0b2f4da71
srinivasallanki <srinivas5859@gmail.com>
2023-06-05 11:25:55 +0530

    button project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className='p-20'>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded'>Count</button>
+    </div>
+  );
+}
+
+export default App;
