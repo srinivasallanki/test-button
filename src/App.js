1f2634b25880961a379a80ee1668819d0d4d444c
srinivasallanki <srinivas5859@gmail.com>
2023-06-05 11:30:00 +0530

    Button changed to red

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className='p-20'>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded'>Count</button>
+      <button className='bg-red-500 text-white px-4 py-2 rounded'>Count</button>
     </div>
   );
 }
